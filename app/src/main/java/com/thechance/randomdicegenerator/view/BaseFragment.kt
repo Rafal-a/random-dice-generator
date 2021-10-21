369057fb1185acc099e9d2757b7236f9275c7a67
Rafal A <rafal.akeel99@gmail.com>
2021-10-21 19:56:48 +0300

    fix base fragment

--- app/src/main/java/com/thechance/randomdicegenerator/view/BaseFragment.kt
+++ app/src/main/java/com/thechance/randomdicegenerator/view/BaseFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB:ViewBinding>(private val  layoutId:Int):Fragment() {
-    private lateinit var _binding :ViewBinding
+abstract class BaseFragment<VB:ViewDataBinding>(private val  layoutId:Int):Fragment() {
+    private lateinit var _binding :ViewDataBinding
      var binding: VB?
      get()=_binding as VB?
     set(value) = TODO()
